d51c630746c15aa1134ac12c1490d3834de6a521
Jamie Patel <jamie@notespublication.com>
2014-10-20 18:03:40 +0100

    User can login with gmail

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,13 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'pusher-angular'])
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic',
+  'pusher-angular',
+  'auth0',
+  'angular-storage',
+  'angular-jwt'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -13,21 +19,27 @@ angular.module('starter', ['ionic', 'pusher-angular'])
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
     if(window.StatusBar) {
+      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider){
+.config(function($stateProvider, $urlRouterProvider, authProvider, $httpProvider,
+  jwtInterceptorProvider) {
 
-  $stateProvider
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+    $stateProvider
 
-    .state('sign_in',{
-      url: "/sign_in",
+     .state('sign_in', {
+      url: '/sign_in',
       views: {
-        'sign_in': {
+        "sign_in": {
           templateUrl: "views/sign_in.html",
-          controller: "LoginCtrl"  
+          controller: "LoginCtrl"   
         }
       }
     })
@@ -40,8 +52,50 @@ angular.module('starter', ['ionic', 'pusher-angular'])
           controller: "ChatCtrl"       
         }
       }
+      // },
+      // data: {
+      //   requiresLogin: true
+      // }
+
     });
 
     $urlRouterProvider.otherwise('/sign_in')
 
+
+  // Configure Auth0
+  authProvider.init({
+    domain: 'pusher-chat.auth0.com',
+    clientID: 'e3a3bcMklZb0TILCkqKIM8PzF6x1AuSi',
+    loginState: 'login'
+  });
+
+
+  jwtInterceptorProvider.tokenGetter = function(store, jwtHelper, auth) {
+    var idToken = store.get('token');
+    var refreshToken = store.get('refreshToken');
+    if (!idToken || !refreshToken) {
+      return null;
+    }
+    if (jwtHelper.isTokenExpired(idToken)) {
+      return auth.refreshIdToken(refreshToken).then(function(idToken) {
+        store.set('token', idToken);
+        return idToken;
+      });
+    } else {
+      return idToken;
+    }
+  }
+
+  $httpProvider.interceptors.push('jwtInterceptor');
+}).run(function($rootScope, auth, store) {
+  $rootScope.$on('$locationChangeStart', function() {
+    if (!auth.isAuthenticated) {
+      var token = store.get('token');
+      if (token) {
+        auth.authenticate(store.get('profile'), token);
+      }
+    }
+
+  });
 });
+
